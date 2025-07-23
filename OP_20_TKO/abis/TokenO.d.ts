e10be9ee706dd4fa1f452b5bbb45e1a4b8064c62
scryptexai <waitlisttt@gmail.com>
2025-07-23 15:50:01 +0000

    deploy

---
+++ OP_20_TKO/abis/TokenO.d.ts
@@ -0,0 +1,42 @@
+import { Address, AddressMap } from '@btc-vision/transaction';
+import { CallResult, OPNetEvent, IOP_NETContract } from 'opnet';
+
+// ------------------------------------------------------------------
+// Event Definitions
+// ------------------------------------------------------------------
+export type MintEvent = {
+    readonly address: Address;
+    readonly amount: bigint;
+};
+
+// ------------------------------------------------------------------
+// Call Results
+// ------------------------------------------------------------------
+
+/**
+ * @description Represents the result of the mint function call.
+ */
+export type Mint = CallResult<
+    {
+        success: boolean;
+    },
+    OPNetEvent<MintEvent>[]
+>;
+
+/**
+ * @description Represents the result of the airdrop function call.
+ */
+export type Airdrop = CallResult<
+    {
+        success: boolean;
+    },
+    OPNetEvent<MintEvent>[]
+>;
+
+// ------------------------------------------------------------------
+// ITokenO
+// ------------------------------------------------------------------
+export interface ITokenO extends IOP_NETContract {
+    mint(address: Address, amount: bigint): Promise<Mint>;
+    airdrop(addressAndAmount: AddressMap<bigint>): Promise<Airdrop>;
+}
